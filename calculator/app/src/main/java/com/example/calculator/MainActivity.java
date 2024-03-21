414805c2c0598e7e7add2eb14717fed96e1ce9de
Badizawr <vicodin-13@ya.ru>
2024-03-21 23:18:18 +0500

    first commit

---
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        EditText editTextAnswer = findViewById(R.id.editTextAnswer);
+        Button buttonAnswer = findViewById(R.id.buttonAnswer);
+        TextView textViewIncorrectAnswer = findViewById(R.id.textViewIncorrectAnswer);
+        TextView textViewCorrectAnswer = findViewById(R.id.textViewCorrectAnswer);
+
+        buttonAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editTextAnswer.getText().toString();
+                if (text.equals("15")) {
+                    textViewCorrectAnswer.setVisibility(View.VISIBLE);
+                    textViewIncorrectAnswer.setVisibility(View.GONE);
+                } else {
+                    textViewIncorrectAnswer.setVisibility(View.VISIBLE);
+                    textViewCorrectAnswer.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+}
