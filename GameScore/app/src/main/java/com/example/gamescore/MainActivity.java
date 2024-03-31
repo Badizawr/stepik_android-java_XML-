f057dc8c182858dc3be2c59108b5261cb7a739c8
Badizawr <vicodin-13@ya.ru>
2024-03-31 23:20:21 +0500

    live Life cycle lesson

--- GameScore/app/src/main/java/com/example/gamescore/MainActivity.java
+++ GameScore/app/src/main/java/com/example/gamescore/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.gamescore;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -16,8 +17,13 @@ public class MainActivity extends AppCompatActivity {
     private int score1 = 0;
     private int score2 = 0;
 
+    private TextView textViewTeam1Score;
+    private TextView textViewTeam2Score;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("MainActivity", "onCreate called");
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -27,30 +33,39 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        textViewTeam1Score = findViewById(R.id.textViewTeam1Score);
+        textViewTeam2Score = findViewById(R.id.textViewTeam2Score);
         if (savedInstanceState != null) {
             score1 = savedInstanceState.getInt("score1");
             score2 = savedInstanceState.getInt("score2");
         }
 
-        TextView textViewTeam1Score = findViewById(R.id.textViewTeam1Score);
-        TextView textViewTeam2Score = findViewById(R.id.textViewTeam2Score);
-
-        textViewTeam1Score.setText(String.valueOf(score1));
-        textViewTeam2Score.setText(String.valueOf(score2));
-
+        updateScore1();
+        updateScore2();
         textViewTeam1Score.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("MainActivity", "Clicked textViewTeam1Score");
                 score1++;
-                textViewTeam1Score.setText(String.valueOf(score1));
+                updateScore1();
             }
         });
         textViewTeam2Score.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textViewTeam2Score.setText(String.valueOf(++score2));
+                Log.d("MainActivity", "Clicked textViewTeam2Score");
+                score2++;
+                updateScore2();
             }
         });
+    }
+
+    private void updateScore1() {
+        textViewTeam1Score.setText(String.valueOf(score1));
+    }
+
+    private void updateScore2() {
+        textViewTeam2Score.setText(String.valueOf(score2));
     }
 
     @Override
@@ -58,5 +73,11 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         outState.putInt("score1", score1);
         outState.putInt("score2", score2);
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("MainActivity", "onDestroy called");
+        super.onDestroy();
     }
 }
