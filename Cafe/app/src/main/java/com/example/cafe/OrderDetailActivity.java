79a0c7de64a2bceaf5a19ac1f83584380cb2997d
Badizawr <vicodin-13@ya.ru>
2024-05-04 23:16:38 +0500

    made the logic of the third screen

--- Cafe/app/src/main/java/com/example/cafe/OrderDetailActivity.java
+++ Cafe/app/src/main/java/com/example/cafe/OrderDetailActivity.java
@@ -3,6 +3,7 @@ package com.example.cafe;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,16 +12,35 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class OrderDetailActivity extends AppCompatActivity {
-    private static  final  String EXTRA_USER_NAME = "userName";
-    private static  final  String EXTRA_DRINK = "drink";
-    private static  final  String EXTRA_DRINK_TYPE = "drinkType";
-    private static  final  String EXTRA_ADDITIVES = "additives";
+    private static final String EXTRA_USER_NAME = "userName";
+    private static final String EXTRA_DRINK = "drink";
+    private static final String EXTRA_DRINK_TYPE = "drinkType";
+    private static final String EXTRA_ADDITIVES = "additives";
+
+    private TextView textViewName;
+    private TextView textViewDrink;
+    private TextView textViewKindOfDrink;
+    private TextView textViewAdditives;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_order_detail);
+        initViews();
+
+        Intent intent = getIntent();
+        textViewName.setText(intent.getStringExtra(EXTRA_USER_NAME));
+        textViewDrink.setText(intent.getStringExtra(EXTRA_DRINK));
+        textViewKindOfDrink.setText(intent.getStringExtra(EXTRA_DRINK_TYPE));
+        textViewAdditives.setText(intent.getStringExtra(EXTRA_ADDITIVES));
+    }
+
+    private void initViews() {
+        textViewName = findViewById(R.id.textViewName);
+        textViewDrink = findViewById(R.id.textViewDrink);
+        textViewKindOfDrink = findViewById(R.id.textViewKindOfDrink);
+        textViewAdditives = findViewById(R.id.textViewAdditives);
     }
 
     public static Intent newIntent(
@@ -29,7 +49,7 @@ public class OrderDetailActivity extends AppCompatActivity {
             String drink,
             String drinkType,
             String additives
-            ) {
+    ) {
         Intent intent = new Intent(context, OrderDetailActivity.class);
         intent.putExtra(EXTRA_USER_NAME, userName);
         intent.putExtra(EXTRA_DRINK, drink);
