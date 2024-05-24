a15b58938b94911b23ab0523d6c2452a544c8384
Badizawr <vicodin-13@ya.ru>
2024-05-24 23:23:06 +0500

    created Dao interface

---
+++ ToDoApp/app/src/main/java/com/example/todoapp_java/NotesDao.java
@@ -0,0 +1,20 @@
+package com.example.todoapp_java;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface NotesDao {
+    @Query("SELECT * FROM notes")
+    List<Note> getNotes();
+
+    @Insert
+    void add(Note note);
+
+    @Query("DELETE FROM notes WHERE id = :id")
+    void remove(int id);
+}
